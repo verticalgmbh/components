1b905a299a13ebe1eaa2aa6bf0487cbf3ee3d4ce
Markus Kollers <m.kollers@vertical.de>
2019-09-18 14:14:47 +0200

    Remove default components

--- projects/vertical-components/src/public-api.ts
+++ projects/vertical-components/src/public-api.ts
@@ -2,6 +2,3 @@
  * Public API Surface of vertical-components
  */
 
-export * from './lib/vertical-components.service';
-export * from './lib/vertical-components.component';
-export * from './lib/vertical-components.module';
