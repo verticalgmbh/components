2365b8fbdfdd20da5c2580cab4db495f14346954
Cederic Privat <cederic.privat@gmail.com>
2019-11-07 17:58:15 +0100

    Fix release script commit message

--- release-script.sh
+++ release-script.sh
@@ -1,11 +1,10 @@
 git checkout -b release-$1 develop
 versionBmpPrj=$(npm version $1)
-echo "Changed project package.json to ${versionBmpMain}"
+echo "Changed project package.json to ${versionBmpPrj}"
 cd projects/vertical-components
 versionBmpLib=$(npm version $1)
 echo "Changed library package.json to ${versionBmpLib}"
 cd ../..
-commitMsg=$(git commit -a -m "Bump version number to $1")
 echo "Create new commit"
-echo $commitMsg
-echo "Release script done"
+git commit -a -m "Bump version number to $1"
+echo "\nRelease script done"
