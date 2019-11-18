2928c6adcf8db06e466ff736e1a034d0a5e30f28
Cederic Privat <cederic.privat@gmail.com>
2019-11-18 16:49:56 +0100

    Add invalid types to vertical input

--- projects/vertical-components/src/input/input.ts
+++ projects/vertical-components/src/input/input.ts
@@ -1,6 +1,18 @@
 
 import { ChangeDetectionStrategy, Component, HostListener, EventEmitter, Input, ElementRef } from '@angular/core';
 
+const VERTICAL_INPUT_INVALID_TYPES = [
+  'button',
+  'checkbox',
+  'file',
+  'hidden',
+  'image',
+  'radio',
+  'range',
+  'reset',
+  'submit'
+];
+
 @Component({
   selector: 'input[vertical-input]',
   templateUrl: 'input.html',
@@ -13,13 +25,28 @@ import { ChangeDetectionStrategy, Component, HostListener, EventEmitter, Input, 
 
 export class VerticalInput {
 
+  @Input()
+  get type(): string { return this._type; }
+  set type(value: string) {
+    this._type = value || 'text';
+    this._validateType();
+    (this._elementRef.nativeElement as HTMLInputElement).type = this._type;
+  };
+
+  protected _type = 'text';
   disabled: boolean;
   focus = new EventEmitter<boolean>();
   @HostListener('focus') _focus = () => this.focus.emit(true);
   @HostListener('blur') _focusLost = () => this.focus.emit(false);
 
-  constructor(public elementRef: ElementRef) {
+  constructor(private _elementRef: ElementRef) {
     // Set disabled to HTML attribute disabled
-    this.disabled = elementRef.nativeElement.hasAttribute('disabled');
+    this.disabled = _elementRef.nativeElement.hasAttribute('disabled');
+  }
+
+  protected _validateType() {
+    if (VERTICAL_INPUT_INVALID_TYPES.indexOf(this._type) > -1) {
+      throw Error(`Input type "${this._type}" isn't supported by vertical-input.`);
+    }
   }
 }
