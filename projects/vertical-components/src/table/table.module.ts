779655a88835df7574b0b1f0b9379ac6512b8f36
Cederic Privat <cederic.privat@gmail.com>
2019-12-19 17:42:34 +0100

    Add table styles

--- projects/vertical-components/src/table/table.module.ts
+++ projects/vertical-components/src/table/table.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { VerticalTable, HeaderRowOutlet } from './table';
 import { VerticalCaption, VerticalHeaderCell, VerticalCell, VerticalCellDef, VerticalColumnDef, VerticalHeaderCellDef } from './directives/cell';
 import { VerticalHeaderRowDef } from './directives/row';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [VerticalTable, VerticalCaption, VerticalHeaderCell, VerticalCell, VerticalCellDef, HeaderRowOutlet, VerticalColumnDef, VerticalHeaderCellDef, VerticalHeaderRowDef],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [VerticalTable, VerticalCaption, VerticalHeaderCell, VerticalCell, VerticalCellDef, HeaderRowOutlet, VerticalColumnDef, VerticalHeaderCellDef, VerticalHeaderRowDef]
 })
