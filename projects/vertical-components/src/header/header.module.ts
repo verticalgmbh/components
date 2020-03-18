d859246d61ead7538a518174c51c92ed64d07dee
Markus Kollers <m.kollers@vertical.de>
2020-03-18 17:55:10 +0100

    Make header really stupid

--- projects/vertical-components/src/header/header.module.ts
+++ projects/vertical-components/src/header/header.module.ts
@@ -1,14 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 
 import { VerticalHeader } from './header';
 
 @NgModule({
-  declarations: [VerticalHeader,],
+  declarations: [VerticalHeader],
   imports: [
-    CommonModule,
-    MatIconModule
+    CommonModule
   ],
   exports: [VerticalHeader]
 })
