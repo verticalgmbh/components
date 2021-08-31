fcb15a9b80aa78bb8b056242e6a5c6b83779c42e
Johannes Gabele <johannesgabele@web.de>
2021-08-31 14:57:02 +0200

    Fix further tests

--- projects/vertical-components/src/test.ts
+++ projects/vertical-components/src/test.ts
@@ -7,6 +7,7 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
+import { instance, mock } from 'ts-mockito';
 
 declare const require: any;
 
@@ -19,3 +20,14 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
+// Helper to more structre ts mockito tests
+export class Mock<T> {
+  mock: T;
+  instance: T;
+
+  constructor() {
+    this.mock = mock<T>();
+    this.instance = instance(this.mock);
+  }
+}
